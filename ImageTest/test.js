a2107359f87fddc1731fdecc71b72353d33d1647
Yooinhak <yih@wjtb.net>
2024-02-08 10:44:12 +0900

    이미지변경

--- ImageTest/test.js
+++ ImageTest/test.js
@@ -1,30 +1,23 @@
-var index = 1;
-var pngname = "64.png";
-var fontexts = document.getElementById("imgText"); // ID가 'imgText'인 요소
+var index =1;
+var pngname = "2.png";
+var imgs = document.getElementById("img");
+var fontexts = document.getElementById("textid");
 
-function NextImage() {
-  ImageChange(1);
-  document.getElementById("img").src = "./images/" + pngname; // 클래스 사용
-
-    fontexts.innerText = pngname; // 텍스트 변경
-
+function NextImage() {     
+ImageChange(1);
 }
-
 function BackImage() {
-  ImageChange(-1);
+ImageChange(-1);
+
 }
 
-function ImageChange(value) {
-  index += value;
 
-  // 예시: 이미지 이름을 변경하는 로직
-  pngname = index + ".png"; // 이미지 이름을 index에 따라 변경
-
-  // switch 문을 사용해 특정 조건에 따른 동작을 추가할 수 있음
-  switch (index) {
-    case 1:
-      // index가 1일 때의 동작
-      break;
-    // 추가적인 케이스
-  }
+function ImageChange(value)
+{
+    index += value;
+    if(index>5)index=2;
+    if(index <2)index = 5;
+ document.getElementById("img").src = "./images/" + index.toString() + ".png";
+  //
+document.getElementById("textid").textContent = index;
 }
